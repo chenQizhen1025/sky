894b8f0c140b84c05f3e17bce4348a01cb2b182a
Chen <2598657731@qq.com>
2026-09-11 17:35:14 +0800

    jwt实现原理，以及通过threadlocal动态获取userid实现

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -81,10 +82,10 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
         //设置创建人和修改人
-        //TODO 后期修改为当前登录用户id，动态获取
-        employee.setCreateUser(10L);
-        employee.setUpdateUser(10L);
-
+        //后期修改为当前登录用户id，动态获取
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        BaseContext.removeCurrentId();
         employeeMapper.insert(employee);
 
     }
