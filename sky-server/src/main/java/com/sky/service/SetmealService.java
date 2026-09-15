78999c07cef471aee1f14eefdbc23e4cbad8cd6c
Chen <2598657731@qq.com>
2026-09-15 16:45:54 +0800

    套餐分页查询

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,10 +1,23 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
+import com.sky.result.PageResult;
 import org.springframework.stereotype.Service;
 
 
 public interface SetmealService {
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     */
     public void saveWithDish(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
