ee7d1fd7e173a6074c574b191d842f759fc6eafb
Chen <2598657731@qq.com>
2026-09-20 18:06:15 +0800

    实现统计不同状态下订单数量功能

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -40,4 +40,7 @@ public interface OrderMapper {
      */
     @Select("select * from orders where id=#{id}")
     Orders getById(Long id);
+
+    @Select("select count(id) from orders where status = #{confirmed}")
+    Integer countStatus(Integer confirmed);
 }
